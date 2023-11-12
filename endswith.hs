898960b716da84339154905026ec9cf4ac893886
Peter Samper <psamper@protonmail.com>
2023-11-11 19:43:04 -0500

    Add docs for endswith

--- endswith.hs
+++ endswith.hs
@@ -1,12 +1,12 @@
+-- KATA: https://www.codewars.com/kata/51f2d1cafc9c0f745c00037d
+-- Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+-- Examples:
+
+-- solution('abc', 'bc') // returns true
+-- solution('abc', 'd') // returns false
+
 module EndsWith where
-
--- endStartingIndex :: String -> String -> Int
--- endStartingIndex str substr = length str - length substr
-
--- solution :: String -> String -> Bool
--- solution _ "" = True
--- solution "" _ = False
--- solution str end = drop (endStartingIndex str end) str == end
 
 -- Drop up to `end` starting index and check if matches `end`
 solution :: String -> String -> Bool
