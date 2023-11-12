f901f67adf8e65a6dd9c46ca4c44d11e36130848
Peter Samper <psamper@protonmail.com>
2023-11-11 19:38:21 -0500

    Add solution and docs for banjo

--- banjo.hs
+++ banjo.hs
@@ -1,13 +1,22 @@
+-- QUESTION: https://www.codewars.com/kata/53af2b8861023f1d88000832
+
+-- Create a function which answers the question "Are you playing banjo?".
+-- If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+-- The function takes a name as its only argument, and returns one of the following strings:
+
+-- name + " plays banjo" 
+-- name + " does not play banjo"
+
+-- Names given are always valid strings.
+
+
 module Banjo where
 
+startsWithR :: String -> Bool
+startsWithR string = take 1 string == "R" || take 1 string == "r"
+
 areYouPlayingBanjo :: String -> String
-areYouPlayingBanjo name@(c:_)
-    | c `elem` "rR" = name ++ " plays banjo"
-    | otherwise = name ++ " does not play banjo"
-
-
--- areYouPlayingBanjo name = name ++ playStr name ++ " banjo"
---     where
---         playStr ('r':_) = " plays"
---         playStr ('R':_) = " plays"
---         playStr _ = " does not play"
+areYouPlayingBanjo name = if take 1 name == "R" || take 1 name == "r"
+                                    then name ++ " plays banjo"
+                                    else name ++ " does not play banjo"
