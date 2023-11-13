60f26d316f7e7d14bf7dc8d6822239957b8989d2
Peter Samper <psamper@protonmail.com>
2023-11-12 19:26:07 -0500

    Add getsum

--- getsum.hs
+++ getsum.hs
@@ -1,5 +1,22 @@
+-- KATA: https://www.codewars.com/kata/55f2b110f61eb01779000053
+-- Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+-- Note: a and b are not ordered!
+-- Examples (a, b) --> output (explanation)
+
+-- (1, 0) --> 1 (1 + 0 = 1)
+-- (1, 2) --> 3 (1 + 2 = 3)
+-- (0, 1) --> 1 (0 + 1 = 1)
+-- (1, 1) --> 1 (1 since both are same)
+-- (-1, 0) --> -1 (-1 + 0 = -1)
+-- (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
+
+-- Your function should only return a number, not the explanation about how you get that number.
+
 module GetSum where
 
 getSum :: Int -> Int -> Int
-getSum a b | a > b = sum [b..a]
-           | otherwise = sum [a..b]
+getSum a b = 
+  if a < b
+  then sum [a..b]
+  else sum [b..a]
